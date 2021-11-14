72a23c5f303a95dbf590e2ab4c9dcde27bf670db
Dongmin Shin <newdm2000@gmail.com>
2021-11-15 02:06:28 +0900

    2021-11-15 02:26 setting

---
+++ Physical_Interaction_Tamagotchi.ino
@@ -0,0 +1,13 @@
+#include "setting.h"
+#include "main.h"
+#include <DHT.h>
+
+void setup()
+{
+
+}
+
+void loop()
+{
+	
+}
---
+++ setting.h
@@ -0,0 +1,18 @@
+//setiing
+//setting Huminity
+#define MAX_HUMINITY 80
+#define MIN_HUMINITY 20
+
+//setting temperature
+#define MAX_TEMPERATURE 28
+#define MIN_TEMPERATURE 15
+
+//setting sound
+#define MAX_SOUND 80
+
+//setting battery
+#define MIN_BATTERY 30
+
+//setting illuminance
+#define MAX_ILLUMINANCE 100
+#define MIN_ILLUMINANCE 20
